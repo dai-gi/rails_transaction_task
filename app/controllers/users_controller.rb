7da8030c0f3dd0ab9a89e2fbb60a7b49cacc493a
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-10-01 12:56:37 +0900

    Add: 悲観的ロック実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      log_in(@user)
-      redirect_to new_order_path
-    else
-      render :new
+    ActiveRecord::Base.transaction do
+      @user = User.lock.new(user_params)
+      if @user.save
+        log_in(@user)
+        redirect_to new_order_path
+      else
+        render :new
+      end
     end
   end
 
